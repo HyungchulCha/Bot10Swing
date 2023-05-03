9f44aac06d07a1428b2b842d495356e72674b02d
ckgudcjf <ckgudcjf@gmail.com>
2023-05-03 16:31:32 +0900

    ing

--- Bot10Swing.py
+++ Bot10Swing.py
@@ -454,7 +454,7 @@ class Bot10Swing():
 if __name__ == '__main__':
 
     B10 = Bot10Swing()
-    # B10.market_to_excel(False, True)
+    B10.market_to_excel(True, True)
 
     while True:
 
@@ -465,7 +465,7 @@ if __name__ == '__main__':
             t_091000 = t_n.replace(hour=9, minute=10, second=0)
             t_152500 = t_n.replace(hour=15, minute=25, second=0)
             t_153000 = t_n.replace(hour=15, minute=30, second=0)
-            t_200000 = t_n.replace(hour=20, minute=0, second=0)
+            t_160000 = t_n.replace(hour=16, minute=0, second=0)
 
             if t_n >= t_085000 and t_n <= t_153000 and B10.bool_marketday == False:
                 if os.path.isfile(os.getcwd() + '/token.dat'):
@@ -485,7 +485,7 @@ if __name__ == '__main__':
                     B10.stock_order()
                     B10.bool_stockorder = True
 
-            if t_n == t_200000 and B10.bool_marketday_end == False:
+            if t_n == t_160000 and B10.bool_marketday_end == False:
 
                 if B10.init_marketday == 'Y':
                     B10.market_to_excel(True, True)
