6bb820e4a81c23c4e7754e625682df238b45b7d9
ckgudcjf <ckgudcjf@gmail.com>
2023-05-06 08:35:48 +0900

    ing

--- Bot10Swing.py
+++ Bot10Swing.py
@@ -397,11 +397,11 @@ if __name__ == '__main__':
             t_160000 = t_n.replace(hour=16, minute=0, second=0)
 
             if t_n >= t_083000 and t_n <= t_153000 and B10.bool_marketday == False:
-                if B10.bkk.fetch_marketday() == 'Y':
-                    B10.market_to_excel()
                 if os.path.isfile(os.getcwd() + '/token.dat'):
                     os.remove('token.dat')
                 B10.init_per_day()
+                if B10.init_marketday == 'Y':
+                    B10.market_to_excel()
                 B10.bool_marketday = True
                 B10.bool_marketday_end = False
 
